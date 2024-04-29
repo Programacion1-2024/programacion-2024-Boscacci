c0f67763065e1a149b0bb9493283a79566d6f19a
EmanuelBoscacci <boscacciemanuel@gmail.com>
2024-04-29 19:59:30 -0300

    commit 1

--- Programacion2024/obj/Release/net8.0/Programacion2024.AssemblyInfo.cs
+++ Programacion2024/obj/Release/net8.0/Programacion2024.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("Programacion2024")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3478380f14649ff015b525a30ca4739b7ec8e16c")]
 [assembly: System.Reflection.AssemblyProductAttribute("Programacion2024")]
